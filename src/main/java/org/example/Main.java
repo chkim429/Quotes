3f20670ef5aecab5040eea53a35b87be8ecaec67
chkim429 <ckdgjs000@naver.com>
2023-03-02 09:55:58 +0900

    2단계 완료

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,15 +6,23 @@ import java.util.stream.Collectors;
 class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String cmd;
+        String cmd, quote, author;
+
+        System.out.println("== 명언 앱 ==");
 
         while (true) {
-            System.out.println("== 명언 앱 ==");
             System.out.printf("명령) ");
-            cmd = sc.nextLine();
+            cmd = sc.nextLine().trim();
 
         if (cmd.equals("종료")) {
             break;
+        }
+        else if(cmd.equals("등록")){
+            System.out.printf("명언 : ");
+            quote = sc.nextLine().trim();
+            System.out.printf("작가 : ");
+            author = sc.nextLine().trim();
+
         }
 
      }
