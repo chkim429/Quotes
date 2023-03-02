f483074da5e07ad63f2cac931a2d3e9faf1fb3aa
chkim429 <ckdgjs000@naver.com>
2023-03-02 10:43:14 +0900

    5단계 완료

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,6 +11,7 @@ class Main {
         System.out.println("== 명언 앱 ==");
 
         int lastId = 0;
+        List<WiseSaying> WiseSayings = new ArrayList<>();
 
         while (true) {
             System.out.printf("명령) ");
@@ -24,12 +25,34 @@ class Main {
             quote = sc.nextLine().trim();
             System.out.printf("작가 : ");
             author = sc.nextLine().trim();
-            int id = ++lastId;
-            System.out.println(id + "번 명언이 등록되었습니다.");
+            int id = lastId + 1;
 
+            WiseSaying wiseSaying = new WiseSaying(id, quote, author);
+            WiseSayings.add(wiseSaying);
+            System.out.println(id + "번 명언이 등록되었습니다.");
+            lastId = id;
+        }
+        else if(cmd.equals("목록")){
+            System.out.println("번호 / 작가 / 명언");
+            System.out.println("----------------------------");
+            for ( int i = WiseSayings.size() - 1; i >=0; i--){
+                WiseSaying wiseSaying = WiseSayings.get(i);
+                System.out.printf("%d / %s / %s\n", wiseSaying.id, wiseSaying.author, wiseSaying.quote);
+            }
         }
 
      }
     }
 
+}
+class WiseSaying{
+    int id;
+    String quote;
+    String author;
+
+    public WiseSaying(int id, String quote, String author) {
+        this.id = id;
+        this.quote = quote;
+        this.author = author;
+    }
 }
