bcf767ee2c10e96766bd7417ea8dcbfbe78c5521
chkim429 <ckdgjs000@naver.com>
2023-03-02 09:34:55 +0900

    1단계

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,20 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String cmd;
+
+        while (true) {
+            System.out.println("== 명언 앱 ==");
+            System.out.printf("명령) ");
+            cmd = sc.nextLine();
+
+        if (cmd.equals("종료")) {
+            break;
+        }
+
+     }
+    }
 
 }
